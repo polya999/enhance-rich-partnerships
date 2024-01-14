978a8672f576dfd2d704a708656f9a53dbac4c3a
Polina <p9991820@gmail.com>
2024-01-14 07:21:30 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,33 @@
+}
+
+    });
+    }
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = generateRandomData();
+
+}
+main();
+    for (let i = 0; i < 10; i++) {
+        console.log("Random Number: " + item);
+    return data;
+    data.forEach(item => {
+function generateRandomData() {
+
+    const data = [];
+function main() {}
+
+main();
+    for (let i = 0; i < 10; i++) {
+function generateRandomData() {
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    return data;
+        console.log("Random Number: " + item);
+}
+    const data = generateRandomData();
+    });
+
+    const data = [];
+    }
+
+function main() {
